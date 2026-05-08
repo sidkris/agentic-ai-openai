c0df4177011118d03c253a960f6cfe451c076394
Siddharth Krishnan <sidkrishnan@protonmail.com>
2026-05-08 23:02:05 +0530

    langsmith connection test

--- langsmith_connection_test.py
+++ langsmith_connection_test.py
@@ -1,6 +1,8 @@
 from langsmith import traceable
 from dotenv import load_dotenv
 import os 
+import pandas as pd
+import yfinance as yf
 
 load_dotenv()
 
@@ -13,4 +15,12 @@ def sample_function(a : int, b : int) -> int:
     return a + b 
 
 
+@traceable
+def get_price_history(ticker : str, start : str = "2026-01-01") -> pd.DataFrame:
+    stock = yf.Ticker(ticker)
+    prices = stock.history(start = start)
+    return prices
+
+
 print(sample_function(10, 11))
+print(get_price_history(ticker = "AAPL"))
