5c1199aab946aa79f2d56d7136470fc269e1f585
Siddharth Krishnan <sidkrishnan@protonmail.com>
2026-05-09 11:10:33 +0530

    web search

--- AGENTS/web_search.py
+++ AGENTS/web_search.py
@@ -1,19 +1,27 @@
 from tavily import TavilyClient
 from dotenv import load_dotenv
 import os
+from langsmith import traceable
 
 load_dotenv()
 
 
 client = TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
 
-response = client.search(
-    query="Latest News",
-    max_results=5
-)
+@traceable
+def search(search_term : str):
 
-for result in response["results"]:
-    print("Title:", result["title"])
-    print("URL:", result["url"])
-    print("Content:", result["content"])
-    print("-" * 50)
+    response = client.search(
+        query="Latest News",
+        max_results=5
+    )
+
+    for result in response["results"]:
+        print("Title:", result["title"])
+        print("URL:", result["url"])
+        print("Content:", result["content"])
+        print("-" * 50)
+
+    return response 
+
+print(search("latest news"))
