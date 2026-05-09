232570ce8910421b7dedba4e9126b8ddaafd4b5b
Siddharth Krishnan <sidkrishnan@protonmail.com>
2026-05-09 23:50:42 +0530

    agents (web search, orchestration)

--- AGENTS/orchestration_layer.py
+++ AGENTS/orchestration_layer.py
@@ -1,13 +1,25 @@
 from web_search import WebSearch
 from dotenv import load_dotenv
-from typing import Dict
+from typing import Dict, Callable
 from openai import OpenAI
 from langsmith import traceable
+import json 
 
 load_dotenv()
 client = OpenAI()
 
+
+AVAILABLE_TOOLS = {
+        "web_search" : WebSearch.search
+}
+
+TOOL_DESCRIPTIONS = """
+        web_search : Use for internet/web search queries
+        """
+
+
 class Orchestration:
+
     @classmethod
     @traceable(name = "tool-selection")
     def pick_tool(cls, user_query : str) -> Dict:
@@ -16,8 +28,8 @@ class Orchestration:
                     Given the following task, suggest the right tool from the ones available, as a JSON object
                     (with no additional text, introduction or explanations)
                     
-                    Available tools :
-                    {WebSearch.search} : for web search tasks
+                    Dictionary of Available tools :
+                    {TOOL_DESCRIPTIONS}
 
                     User task : 
                     {user_query}
@@ -33,8 +45,26 @@ class Orchestration:
         )
 
 
-        return response.choices[0].message.content
+        response = response.choices[0].message.content
+        return json.loads(response)
+
+    @classmethod
+    @traceable(name = "tool-execution")    
+    def execute_tool(cls, tool_response: Dict):
+
+        tool_name = tool_response["suggested_tool"]
+        search_query = tool_response["task"]
+
+        if tool_name not in AVAILABLE_TOOLS:
+            raise ValueError(f"Unknown tool: {tool_name}")
+
+        selected_tool = AVAILABLE_TOOLS[tool_name]
+
+        return selected_tool(search_query)
 
 
-tool_selection = Orchestration.pick_tool(user_query = "I want to search for the term 'bitcoin'")
-print(tool_selection)
+if __name__ == "__main__":
+
+    tool_selection = Orchestration.pick_tool(user_query = "history of bitcoin")
+    print(tool_selection)
+    print(Orchestration.execute_tool(tool_selection))
